e2be4e28e93bf4f89260ef6f534b24288b8bbc4f
Sibela Chinareva <sibela.chinareva@chaos.com>
2024-11-13 15:49:39 +0200

    Setting up main application, dll and entry point

---
+++ Sandbox/src/SandboxApp.cpp
@@ -0,0 +1,17 @@
+#include <Nutcracker.h>
+
+class Sandbox : public Nutcracker::Application
+{
+public:
+	Sandbox() {
+		
+	}
+	~Sandbox() {
+		
+	}
+};
+
+Nutcracker::Application* Nutcracker::CreateApplication()
+{
+	return new Sandbox();
+}
---
+++ nutcracker/src/Nutcracker.h
@@ -0,0 +1,9 @@
+#pragma once
+
+// For use by Nutcracker applications
+
+#include "Nutcracker/Application.h"
+
+// ---------------------------Entry Point---------------------------
+#include "Nutcracker/EntryPoint.h"
+// -----------------------------------------------------------------
